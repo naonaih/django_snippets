98fe0424517bdced15bac69de9d5bd26b98a43c9
Naoya Hara <naonaih@gmail.com>
2021-10-17 22:01:25 +0900

    Added Comment Function

--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse,HttpResponseForbidden
-from snippets.models import Snippet
-
-from snippets.forms import SnippetForm
+from snippets.models import Snippet, Comment
+from snippets.forms import SnippetForm,CommentForm
 # Create your views here.
+
 def top(request):
 	snippets = Snippet.objects.all()
 	context = {"snippets": snippets}
 	#return HttpResponse(b"Hello World")
 	return render(request, "snippets/top.html", context)
+
 
 @login_required
 def snippets_new(request):
@@ -23,6 +24,7 @@ def snippets_new(request):
 	else:
 		form = SnippetForm()
 	return render(request, "snippets/snippet_new.html", {'form': form})
+
 
 @login_required
 def snippets_edit(request, snippet_id):
@@ -39,6 +41,25 @@ def snippets_edit(request, snippet_id):
 		form = SnippetForm(instance=snippet)
 	return render(request, "snippets/snippet_edit.html", {'form': form})
 
+
 def snippets_detail(request, snippet_id):
 	snippet = get_object_or_404(Snippet, pk=snippet_id)
-	return render(request, "snippets/snippet_detail.html",{'snippet':snippet})
+	try:
+		comments = Comment.objects.filter(commented_to=snippet)
+	except Exception as e:
+		comments = None
+	#print(comments)
+	#print(snippet)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.commented_by = request.user
+			comment.commented_to = snippet
+			comment.save()
+			#Formを白紙にするため
+			form = CommentForm()
+	else:
+		form = CommentForm()
+	return render(request, "snippets/snippet_detail.html",\
+		{'snippet':snippet,'comments':comments, 'form':form})
